c4084f16a2b8f017e0957b1029e2dd98d753ac1f
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2019-02-03 19:44:35 +0530

    Standard checkout works

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def inline_checkout
+  end
+
+  def standard_checkout
+  end
+
+  def order_processed
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+  get '/home', to: 'home#index'
+  get "/inline-checkout", to: "home#inline_checkout", as: :inline_checkout
+  get "/standard-checkout", to: "home#standard_checkout", as: :standard_checkout
+  get "/2checkout/order-processed", to: "home#order_processed", as: :order_processed
+
 end
